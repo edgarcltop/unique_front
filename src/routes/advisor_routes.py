72233c41ab1fc827f92afc4c21f4dbf2dd7ecb33
edgarcltop <edgarclcace@gmail.com>
2015-06-24 22:37:44 +0100

    added the advisor routes

---
+++ src/routes/advisor_routes.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template, abort
+main = Blueprint("main", __name__)
+
+@main.route("/advisor/<username>")
+def advisor(username):
+    return render_template("advisor.html", username=username)
+
+@main.route("/advisor/settings/<username>")
+def advisor_settings(username):
+    return render_template("advisor_settings.html", username=username)
