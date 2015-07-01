04d52e9a49ae31c653d3e21c5a12c8d7aad0f9ea
edgarcltop <edgarclcace@gmail.com>
2015-07-01 22:43:15 +0100

    added gradient  routes

---
+++ src/routes/optimiser_routes.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template, abort
+optimiser = Blueprint("optimiser", __name__)
+
+@optimiser.route("/optimiser")
+def optimiser_route():
+    return render_template("optimiser.html")
