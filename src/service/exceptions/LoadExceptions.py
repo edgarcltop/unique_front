e207f2f2826ce75d8816d21a2a5437a0e2b0c622
edgarcltop <edgarclcace@gmail.com>
2015-08-02 22:56:51 +0100

    graded level

--- src/service/exceptions/LoadExceptions.py
+++ src/service/exceptions/LoadExceptions.py
@@ -2,4 +2,11 @@
 class LoadExceptions(Exception):
     def __init__(self, message):
         self.message = message
+        super().__init__(self.message)
 
+    def __str__(self):
+        return f"Load Exception: {self.message}"
+    
+    def __repr__(self):
+        return f"LoadExceptions(message={self.message})"
+    
