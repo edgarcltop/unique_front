2a810c552fd4b6cceaf58a46149b30882628786d
edgarcltop <edgarclcace@gmail.com>
2015-08-08 22:58:44 +0100

    activated user service

---
+++ src/service/implement/UserService.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from src.models.User import User
+from src.repository.UserRepository import UserRepository
+
+class UserService(ABC):
+    @abstractmethod
+    def create_user(self, username: str, email: str, password: str) -> User:
+        pass
+
+    @abstractmethod
+    def get_user_by_id(self, user_id: int) -> Optional[User]:
+        pass
+
+    @abstractmethod
+    def get_all_users(self) -> List[User]:
+        pass
---
+++ src/service/models/User.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, user_id: int, username: str, email: str):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
