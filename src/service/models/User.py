315baca900930c9056842ffe30c10cf2cb7e6524
edgarcltop <edgarclcace@gmail.com>
2015-08-09 22:59:09 +0100

    activated user service

--- src/service/models/User.py
+++ src/service/models/User.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, user_id: int, username: str, email: str):
+    def __init__(self, user_id: int, username: str, email: str, password: str):
         self.user_id = user_id
         self.username = username
         self.email = email
+        self.password = password
