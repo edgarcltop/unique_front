ebf0df984980a722dde457f805954c304b369e9a
edgarcltop <edgarclcace@gmail.com>
2015-08-10 23:00:06 +0100

    activated user service

--- src/service/models/User.py
+++ src/service/models/User.py
@@ -4,3 +4,13 @@ class User:
         self.username = username
         self.email = email
         self.password = password
+    def __repr__(self):
+        return f"User(user_id={self.user_id}, username={self.username}, email={self.email}, password={self.password})"
+    
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password
+        }
