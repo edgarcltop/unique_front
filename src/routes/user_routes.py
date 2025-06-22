48ca864c896c1127a7d370af71a81a9fe4e2b879
edgarcltop <edgarclcace@gmail.com>
2025-06-22 22:10:23 +0100

    added the registration functions

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,9 +1,23 @@
 from flask import Blueprint, render_template, abort
 main = Blueprint("main", __name__)
 
-@main.route("/<username>")
-def profile(username):
-    if username == "admin":
-        return render_template("admin.html")
-    else:
-        abort(404)
+@main.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+@main.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+@main.route("/me")
+def profile():
+    return render_template("profile.html")
+
+@main.route("/verify")
+def verify():
+    return render_template("verify.html")
