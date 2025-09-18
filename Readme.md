4b8d9f9617cf809ccacd445a7b9000ad1f8fcef2
edgarcltop <edgarclcace@gmail.com>
2025-09-18 21:53:04 +0100

    added readme.md

---
+++ Readme.md
@@ -0,0 +1,3 @@
+###Stock Market Picks
+## Handling the stock market prices
+
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+rom flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"y
+
